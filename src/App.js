f9f1ad0cd637ac0634f77383217c66508af0170b
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2021-03-28 23:21:19 -0700

    redux set up and auth action reducer done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Chakra ui
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
 // pages
-import Landing from "./pages/landing/Landing";
+import Landing from './pages/landing/Landing';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <ChakraProvider>
         <Router>
           <Switch>
-            <Route exact path="/" component={Landing} />
+            <Route exact path='/' component={Landing} />
           </Switch>
         </Router>
       </ChakraProvider>
-    </>
+    </Provider>
   );
 }
 
