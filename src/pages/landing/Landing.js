0479e2118ddf3b2c97e4e3e74f35da15fca19fca
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2021-03-29 23:13:51 -0700

    height

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -5,7 +5,6 @@ import {
   Center,
   Grid,
   GridItem,
-  Spacer,
   Divider,
   Button,
   Drawer,
@@ -15,6 +14,7 @@ import {
   DrawerContent,
   useDisclosure,
   Modal,
+  Box,
 } from '@chakra-ui/react';
 //components
 import SignIn from '../../components/signin/SignIn';
@@ -35,76 +35,72 @@ const Landing = ({ isAuthenticated }) => {
 
   return (
     <>
-      <Grid
-        h='00px'
-        templateRows='repeat(5, 1fr)'
-        templateColumns='repeat(5, 1fr)'
-        gap={40}
-      >
-        <GridItem colStart={3} rowStart={4}>
-          <Center h='100px' color='white'>
-            <Button colorScheme='teal' variant='ghost' onClick={onOpen}>
+      <Center margin='auto' style={{ height: '100%' }}>
+        <Button
+          colorScheme='teal'
+          variant='ghost'
+          onClick={onOpen}
+          margin='auto'
+        >
+          <Text fontSize='2xl' as='samp'>
+            Sungmin Market
+          </Text>
+        </Button>
+      </Center>
+      <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerHeader borderBottomWidth='1px'>
               <Text fontSize='2xl' as='samp'>
                 Sungmin Market
               </Text>
-            </Button>
-            <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
-              <DrawerOverlay>
-                <DrawerContent>
-                  <DrawerHeader borderBottomWidth='1px'>
-                    <Text fontSize='2xl' as='samp'>
-                      Sungmin Market
-                    </Text>
-                  </DrawerHeader>
-                  <DrawerBody>
-                    <br></br>
-                    <Button
-                      colorScheme='green'
-                      variant='link'
-                      onClick={() => setSignInModal(true)}
-                      size='lg'
-                    >
-                      Sign in
-                    </Button>
-                    <Modal
-                      size='sm'
-                      initialFocusRef={initialRef}
-                      finalFocusRef={finalRef}
-                      isOpen={signInModal}
-                      onClose={() => setSignInModal(false)}
-                    >
-                      {/* Sign in modal */}
-                      <SignIn />
-                    </Modal>
-                    <br></br>
-                    <br></br>
-                    <Divider />
-                    <br></br>
-                    <Button
-                      colorScheme='green'
-                      variant='link'
-                      onClick={() => setSignUpModal(true)}
-                      size='lg'
-                    >
-                      Sign up
-                    </Button>
-                    <Modal
-                      size='xl'
-                      initialFocusRef={initialRef}
-                      finalFocusRef={finalRef}
-                      isOpen={signUpModal}
-                      onClose={() => setSignUpModal(false)}
-                    >
-                      {/* sign out modal */}
-                      <SignUp />
-                    </Modal>
-                  </DrawerBody>
-                </DrawerContent>
-              </DrawerOverlay>
-            </Drawer>
-          </Center>
-        </GridItem>
-      </Grid>
+            </DrawerHeader>
+            <DrawerBody>
+              <br></br>
+              <Button
+                colorScheme='green'
+                variant='link'
+                onClick={() => setSignInModal(true)}
+                size='lg'
+              >
+                Sign in
+              </Button>
+              <Modal
+                size='sm'
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={signInModal}
+                onClose={() => setSignInModal(false)}
+              >
+                {/* Sign in modal */}
+                <SignIn />
+              </Modal>
+              <br></br>
+              <br></br>
+              <Divider />
+              <br></br>
+              <Button
+                colorScheme='green'
+                variant='link'
+                onClick={() => setSignUpModal(true)}
+                size='lg'
+              >
+                Sign up
+              </Button>
+              <Modal
+                size='xl'
+                initialFocusRef={initialRef}
+                finalFocusRef={finalRef}
+                isOpen={signUpModal}
+                onClose={() => setSignUpModal(false)}
+              >
+                {/* sign out modal */}
+                <SignUp />
+              </Modal>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </>
   );
 };
